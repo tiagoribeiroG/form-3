896eda0f2ac3a3c494bd3f767bb67d80418895d3
Tiago-Ribeiro <tiagoribeiro@gmx.us>
2023-06-21 21:28:16 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const passwordConfirmation = document.getElementById('password-confirmation');
+
+
+form.addEventListener('submit')

5885c3a46c57b46b337a02ec7ea19bf28f91dbac
Tiago-Ribeiro <tiagoribeiro@gmx.us>
2023-06-23 10:34:31 -0300

    js

--- script.js
+++ script.js
@@ -5,4 +5,41 @@ const password = document.getElementById('password');
 const passwordConfirmation = document.getElementById('password-confirmation');
 
 
-form.addEventListener('submit')
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    checkInputs();
+
+});
+
+function checkInputs() {
+    const usernameValue = username.value;
+    const emailValue = email.value;
+    const passwordValue = password.value;
+    const passwordConfirmationValue = passwordConfirmation.value;
+
+    if (usernameValue === "") {
+        setErrorFor(username, "o nome de usuario é obrigatório.");
+    }
+}
+
+function setErrorFor(input, message) {
+    // const formControl = input.parentElement;
+    // formControl.className = 'form-control sucess';
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    //adiciona a classe de erro
+    small.innertext = message;
+
+    //adiciona a classe de erro
+    formControl.classname = 'form-control error';
+    
+
+}
+
+function setSuccesFor(input) {
+    const formControl = input.parentElement;
+    //adicionar class de sucess
+
+    formControl.className = 'form-control sucess';
+}
